e2a4a3a72c8798e926714f30bc471f2a6cbf6c18
Lucas Turci <lucas.turci@gmail.com>
2018-06-05 11:15:04 -0300

    Adicionei a pasta views, onde iremos colocar as paginas que devem ser renderizadas. Refatorei o codigo para configurar o ejs e usar esses views

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-var app = require('express')();
+var app = require('express')(); // criacao do aplicativo
+
+/* Configuracoes iniciais */
+app.set("view engine", "ejs"); // fala que esta usando ejs como motor de views
+app.set("views", "./app/views"); // fala que as views (os arquivos ejs) estao no diretorio ./app/views
+
+/* Fala pra porta 3000 escutar as requisicoes */
 app.listen(3000, function() {
-	console.log("Ola mundo :)")
+	console.log("Ola mundo :)");
 });
 
+
+/* Se uma requisicao get no diretorio '/' for recebida, renderiza a index.ejs (pagina inicial) */
 app.get('/', function(req, res) {
-	res.send('<html> Ola mundo </html>');
+	res.render('index');
 });
